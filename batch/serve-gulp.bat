5f949d80ef8a0af367916949e5f8062302d2e0e3
Reeceau92 <Reeceau92@users.noreply.github.com>
2017-07-03 13:35:58 +1000

    Settings View Initial Design
    Splitting up the settings view on the single page rather then two. This is done through the use of wells. Also allowed user management to be underneath on this page as well.

--- batch/serve-gulp.bat
+++ batch/serve-gulp.bat
@@ -1,4 +1,4 @@
-Powershell.exe -executionpolicy remotesigned -File  \envar.ps1
+Powershell.exe -executionpolicy remotesigned -File  E:\Documents\GitHub\nativeqda-dev\batch\envar.ps1
 start E:\Documents\GitHub\nativeqda-dev\batch\start-mongod.bat
 e:
 cd E:\Documents\GitHub\nativeqda-dev
