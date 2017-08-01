fa1a1d4b6be6922271a891f170765f5489981288
rogerBen <ben.tfpm@gmail.com>
2017-08-01 11:44:40 +1000

    Fixed bar chart, now vertical bar chart of keywords with text labels

--- src/app_client/components/visualisations/visuals/barChart/barChart.controller.js
+++ src/app_client/components/visualisations/visuals/barChart/barChart.controller.js
@@ -29,8 +29,9 @@
 				
 				analysisData.keywords.forEach(function(keyword){
 					var relevance = keyword.relevance*100;
-					
-					data.push(relevance);
+					//var text = "Hello";
+					var text = keyword.text.charAt(0).toUpperCase() + keyword.text.slice(1);	// Capitalise first letter
+					data.push({relevance: keyword.relevance, text: text});
 				});
 				
 				
@@ -42,7 +43,45 @@
 
 		function drawChart(data) {
 			
+			var svg = d3.select("svg"),
+				margin = {top: 20, right: 20, bottom: 30, left: 40},
+    			width = +svg.attr("width") - margin.left - margin.right,
+    			height = +svg.attr("height") - margin.top - margin.bottom; 
 			
+    		var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+    			y = d3.scaleLinear().rangeRound([height, 0]);
+
+			var g = svg.append("g")
+    			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    		x.domain(data.map(function(d) { return d.text; }));
+    		y.domain([0, d3.max(data, function(d) { return d.relevance; })]);
+
+    		g.append("g")
+      			.attr("class", "axis axis--x")
+      			.attr("transform", "translate(0," + height + ")")
+      			.call(d3.axisBottom(x));
+
+
+      		 g.append("g")
+      			.attr("class", "axis axis--y")
+      			.call(d3.axisLeft(y).ticks(10, "%"))
+    			.append("text")
+      			.attr("transform", "rotate(-90)")
+      			.attr("y", 6)
+      			.attr("dy", "0.71em")
+      			.attr("text-anchor", "end")
+      			.text("Relevance");
+
+      		  g.selectAll(".bar")
+    			.data(data)
+    			.enter().append("rect")
+      			.attr("class", "bar")
+      			.attr("x", function(d) { return x(d.text); })
+      			.attr("y", function(d) { return y(d.relevance); })
+      			.attr("width", x.bandwidth())
+      			.attr("height", function(d) { return height - y(d.relevance); });
+    	/*
 		d3.select(".chart")
 			.selectAll("div")
 			.data(data)
@@ -50,6 +89,8 @@
 			.append("div")
 			.style("width", function(d) {return d + "px";})
 			.text(function(d) {return d;});
+
+		*/
 		}
 	}
 
