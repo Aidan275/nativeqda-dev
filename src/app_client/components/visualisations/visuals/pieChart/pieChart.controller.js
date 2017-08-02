34238da6eded65caed303cca8c4c44314ba4102e
rogerBen <ben.tfpm@gmail.com>
2017-08-02 14:13:58 +1000

    Added code for drawing pie chart

--- src/app_client/components/visualisations/visuals/pieChart/pieChart.controller.js
+++ src/app_client/components/visualisations/visuals/pieChart/pieChart.controller.js
@@ -29,7 +29,6 @@
 				
 				analysisData.keywords.forEach(function(keyword){
 					var relevance = keyword.relevance*100;
-					//var text = "Hello";
 					var text = keyword.text.charAt(0).toUpperCase() + keyword.text.slice(1);	// Capitalise first letter
 					data.push({relevance: keyword.relevance, text: text});
 				});
@@ -43,6 +42,42 @@
 
 		function drawChart(data) {
 			
+
+			var svg = d3.select("svg"),
+    		width = +svg.attr("width"),
+    		height = +svg.attr("height"),
+    		radius = Math.min(width, height) / 2,
+    		g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+			var color = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+			var pie = d3.pie()
+    				.sort(null)
+    				.value(function(d) { return d.relevance; });
+
+			var path = d3.arc()
+    				.outerRadius(radius - 10)
+    				.innerRadius(0);
+
+			var label = d3.arc()
+    				.outerRadius(radius - 40)
+    				.innerRadius(radius - 40);
+
+  			var arc = g.selectAll(".arc")
+    				.data(pie(data))
+    				.enter().append("g")
+      				.attr("class", "arc");
+
+  			arc.append("path")
+      				.attr("d", path)
+      				.attr("fill", function(d) { return color(d.data.text); });
+
+  			arc.append("text")
+      				.attr("transform", function(d) { return "translate(" + label.centroid(d) + ")"; })
+      				.attr("dy", "0.35em")
+      				.text(function(d) { return d.data.age; });
+
+
 		}
 	}
 
