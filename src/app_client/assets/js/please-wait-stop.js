13e0e210b7842ee490d5468011c804f188a96e49
Aidan <aa275@uowmail.edu.au>
2017-08-10 15:10:17 +1000

    Fix map sidebar covering dropdown menu on mobiles
    Adjusting sidebar z-index fixed problem

--- src/app_client/assets/js/please-wait-stop.js
+++ src/app_client/assets/js/please-wait-stop.js
@@ -1,3 +1,5 @@
-initial_loading_screen.finish();
+setTimeout(function() {
+	initial_loading_screen.finish();
+}, 1000) /* add 1s delay when scripts have loaded before removing the loading screen to account for any additional AJAX requests happening on the loaded page */
 
 
