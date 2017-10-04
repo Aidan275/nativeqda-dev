302bf839fedd20ad1ae34bceea4f661312e3783e
rogerBen <ben.tfpm@gmail.com>
2017-10-04 14:14:05 +1100

    More network graph

--- src/app_client/components/visualisations/visuals/networkChart/networkChart.controller.js
+++ src/app_client/components/visualisations/visuals/networkChart/networkChart.controller.js
@@ -31,7 +31,8 @@
 						"they", "I", "at", "be", "this", "have", "from", "or", "one",
 						"had", "by", "word", "but", "not", "what", "all", "where", "your",
 						"can", "said", "there", "use", "an", "each", "which", "do", "how", "their", 
-						"if", "will", "Mr", "Ms", "one", "two", "man", "boy"];
+						"if", "will", "Mr", "Ms", "one", "two", "man", "boy", "her", "Mr", "Mr.",
+						"Mrs", "Mrs.", "me", "him", "He", "Her", "Their"];
 
 
 		activate();
@@ -53,7 +54,7 @@
 						argumentA: object.arguments[0].text,
 						argumentB: object.arguments[1].text,
 						score: object.score,
-						common: 0
+						common: false
 					};
 					responseData.push(anObject);
 					repeaterID++;
@@ -70,9 +71,7 @@
 		function tidyData(data) {
 			var tidy = [];
 
-			//Remove common words
-
-			//for each
+			//Mark objects with common words by increasing the common number
 			data.forEach(function(obj){
 				var arrayLength = common.length;
 
@@ -80,18 +79,18 @@
 				for(var i = 0; i < arrayLength; i++) {
 					if(common[i] === obj.argumentA) {
 						//If match dont use
-						obj.common++;
+						//obj.common++;
+						obj.common = true;
 					}
 
 					if(common[i] === obj.argumentB) {
-						obj.common++;
+						//obj.common++;
+						obj.common = true;
 					}
 				}		
 			});
 
-		
-			//Tidy data
-			console.log(analysisName);
+			//If common > 0 then don't use it
 
 			var i = 0;
 
@@ -99,58 +98,135 @@
 			tidy.push({name: analysisName, group: i, weight: 1});
 			i++;
 
+
+
 			data.forEach(function (data) {
 				//If no common words used
-				if(data.common == 0) {
-						var tidyLength = tidy.length;
-						for(var i = 0; i < tidyLength; i++) {
-							if(tidy[i].name == data.argumentA) {
-								//Duplicate
-								data.common = -1;
-							}							
-						}
-						if(data.common == 0) {
-							tidy.push({name: data.argumentA, group: i, weight: data.score});
-							tidy.push({name: data.argumentB, group: i, weight: data.score});
-							i++;
-						}
-					}				
+				if(!data.common) {
+					tidy.push({name: data.argumentA, group: i, weight: data.score});
+					tidy.push({name: data.argumentB, group: i, weight: data.score});
+					i++;
+				}
+				
 			});
 
+
 			//Remove duplicates
-			console.log(data);
 
 			//Make links
 			var links = [];
 
+			
+			
 			tidy.forEach(function (data) {
 				//Make links to root node
-				links.push({source: 0, target: data.group, weight: (data.weight*10)});
+				links.push({source: 0, target: data.group});
 			});
+
 
 			tidy.forEach(function (data) {
-				links.push({source: data.group, target: data.group, weight: (data.weight*10)});
+				links.push({source: data.group, target: data.group});
 			});
 
-
-			console.log(tidy);
-			console.log(links);
  
 			drawChart(tidy, links);
-				
-				
 
+
+			
 
 		}
 
 
 		function drawChart(tidy, links) {
-
-
 			console.log(tidy);
 			console.log(links);
- 
 
+
+	
+
+			var svg = d3.select("svg"),
+			    width = +svg.attr("width"),
+			    height = +svg.attr("height");
+
+			var simulation = d3.forceSimulation()
+			    .force("link", d3.forceLink())
+			    .force("charge", d3.forceManyBody().strength(-400))
+			    .force("center", d3.forceCenter(width / 2, height / 2));
+			  
+			links.forEach(function(d){
+			    d.source = d.source;    
+			    d.target = d.target;
+			});
+
+			var link = svg.append("g")
+			                .style("stroke", "#aaa")
+			                .selectAll("line")
+			                .data(links)
+			                .enter().append("line");
+
+			var node = svg.append("g")
+            			.attr("class", "nodes")
+  			.selectAll("circle")
+            			.data(tidy)
+  			.enter().append("circle")
+          			.attr("r", 6)
+          			.call(d3.drag()
+              			.on("start", dragstarted)
+              			.on("drag", dragged)
+              			.on("end", dragended));
+
+
+            var label = svg.append("g")
+      			.attr("class", "labels")
+      			.selectAll("text")
+      			.data(tidy)
+      			.enter().append("text")
+        			.attr("class", "label")
+        			.text(function(d) { return d.name; })
+
+        	simulation
+      			.nodes(tidy)
+      			.on("tick", ticked);
+
+      		simulation.force("link")
+      			.links(links);
+
+      	  	function ticked() {
+    			link
+        			.attr("x1", function(d) { return d.source.x; })
+        			.attr("y1", function(d) { return d.source.y; })
+        			.attr("x2", function(d) { return d.target.x; })
+        			.attr("y2", function(d) { return d.target.y; });
+
+    			node
+         			.attr("r", 20)
+         			.style("fill", "#d9d9d9")
+         			.style("stroke", "#969696")
+         			.style("stroke-width", "1px")
+         			.attr("cx", function (d) { return d.x+6; })
+         			.attr("cy", function(d) { return d.y-6; });
+    
+   				label
+    				.attr("x", function(d) { return d.x; })
+            		.attr("y", function (d) { return d.y; })
+            		.style("font-size", "14px").style("fill", "#4393c3");
+  			}
+
+
+			function dragstarted(d) {
+  				if (!d3.event.active) simulation.alphaTarget(0.3).restart()
+  				simulation.fix(d);
+			}
+
+			function dragged(d) {
+			  simulation.fix(d, d3.event.x, d3.event.y);
+			}
+
+			function dragended(d) {
+			  if (!d3.event.active) simulation.alphaTarget(0);
+			  simulation.unfix(d);
+			}
+	           			
 		}
 
 }
